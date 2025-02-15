98b5aa6d6be24dab2084e311b03f5bbab20dbebe
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-15 12:41:42 +0530

    Refactor GameLogic methods for improved readability and consistency

--- src/game/GameLogic.java
+++ src/game/GameLogic.java
@@ -5,23 +5,23 @@ public class GameLogic {
     private char currentPlayer;
     private boolean gameWon;
 
-    public GameLogic(){
+    public GameLogic() {
         board = new char[3][3];
         resetBoard();
     }
 
-    public void resetBoard(){
-        for(int i=0;i<3;i++){
-            for(int j=0;j<3;j++){
-                board[i][j]='-';
+    public void resetBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = '-';
             }
         }
         currentPlayer = 'X';
         gameWon = false;
     }
 
-    public boolean makeMove(int row, int col){
-        if(board[i][j]=='-' && !gameWon){
+    public boolean makeMove(int row, int col) {
+        if (board[row][col] == '-' && !gameWon) {
             board[row][col] = currentPlayer;
             gameWon = checkWin();
             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
@@ -29,13 +29,14 @@ public class GameLogic {
         }
         return false;
     }
+
     public boolean checkWin() {
         return checkRows() || checkColumns() || checkDiagonals();
     }
 
     private boolean checkRows() {
-        for(int i=0;i<3;i++){
-            if(board[i][0] != '-' && board[i][0] == board[i][1] &&  board[i][1] == board[i][2]){
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] != '-' && board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
                 return true;
             }
         }
@@ -43,12 +44,24 @@ public class GameLogic {
     }
 
     private boolean checkColumns() {
-        for(int i=0;i<3;i++){
-            if(board[0][i] != '-' && board[0][i] == board[1][i] &&  board[1][i] == board[2][i]){
+        for (int j = 0; j < 3; j++) {
+            if (board[0][j] != '-' && board[0][j] == board[1][j] && board[1][j] == board[2][j]) {
                 return true;
             }
         }
         return false;
     }
-    
+
+    private boolean checkDiagonals() {
+        return (board[0][0] != '-' && board[0][0] == board[1][1] && board[1][1] == board[2][2]) ||
+               (board[0][2] != '-' && board[0][2] == board[1][1] && board[1][1] == board[2][0]);
+    }
+
+    public char getCurrentPlayer() {
+        return currentPlayer;
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
 }
