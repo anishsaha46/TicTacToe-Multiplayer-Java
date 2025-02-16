2af2cc3b3a90a385f7e621461372ed3caa46cdc3
anishsaha46 <anish.ece321046@bppimt.ac.in>
2025-02-16 10:43:40 +0530

    refactor : makeMove function with validity condition and Draw condition

--- src/game/GameLogic.java
+++ src/game/GameLogic.java
@@ -21,9 +21,17 @@ public class GameLogic {
     }
 
     public boolean makeMove(int row, int col) {
+
+        if(row <0 || row >=3 || col <0 || col >=3){
+            return false;
+        }
+
         if (board[row][col] == '-' && !gameWon) {
             board[row][col] = currentPlayer;
             gameWon = checkWin();
+            if(!gameWon && isDraw()){
+                gameWon = true;
+            }
             currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
             return true;
         }
